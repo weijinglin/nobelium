da4b5b8fdb4595eaf3fa0fcc974107525e56b078
jinglinwei <weijinglin@sjtu.edu.cn>
2024-12-21 15:23:47 +0800

    fix blog config

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: "Linyu's Blog",
+  author: 'Linyu',
+  email: 'wellion77777@gmail.com',
   link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  description: '浮云一别后，流水十年间',
+  lang: ['en-US', 'zh-CN'],
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
